9732b81ee96b50a3474d5d0519eb923fa5afe44d
Stephen Lombardo <sjlombardo@zetetic.net>
2009-05-18 22:16:13 -0400

    fix routes to use /blog; fix rendering of decimal dates (not octal!)

--- app/controllers/mephisto_controller.rb
+++ app/controllers/mephisto_controller.rb
@@ -1,7 +1,7 @@
 class MephistoController < ActionController::Base
   
   def redirect
-    redirect_to url_for("/articles/#{params[:year]}/#{sprintf("%02d",params[:month])}/#{sprintf("%02d",params[:day])}/#{params[:slug]}/"), :status=>:moved_permanently
+    redirect_to url_for("/blog/#{params[:year]}/#{sprintf("%02d",params[:month].to_i)}/#{sprintf("%02d",params[:day].to_i)}/#{params[:slug]}/"), :status=>:moved_permanently
   end
   
 end  
